42720b14bbebec81da703c2ac5786673f6d657ec
luxscious <ggerges019@gmail.com>
2020-09-29 19:00:54 -0400

    am not sure what im doing
    pls help

--- Shapes/Main.js
+++ Shapes/Main.js
@@ -1,3 +1,15 @@
 
-let x = 2;
-console.log(x);
+
+function pallet(displacement){
+    
+    function drawShape(choice){
+        switch(choice){
+            case 1: //rectangle
+                drawRectangle();
+            case 2: //Triangle
+                drawTriangle();
+        }
+    }
+}
+let offset = 2;
+pallet(offset);
